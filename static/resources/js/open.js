f73ff137cc19625e03db09004a7dec6d73cce60c
Dex <82985536+BinaryDex@users.noreply.github.com>
2023-11-19 22:08:00 -0500

    gonna shoot myself

--- static/resources/js/open.js
+++ static/resources/js/open.js
@@ -1,4 +1,11 @@
 function open(url) {
-    localStorage.setItem('savedURL', url)
-    window.location.href = 'open';
+    var newWindow = window.open("", "_blank");
+
+    var iframe = document.createElement("iframe");
+    iframe.src = url;
+    iframe.style.width = "100%";
+    iframe.style.height = "100%";
+    iframe.style.border = "none";
+
+    newWindow.document.body.appendChild(iframe);
 }
